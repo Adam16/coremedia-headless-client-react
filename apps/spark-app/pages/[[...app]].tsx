b195d8558b5dd14743e5d0b28cdbccf2ec7e4afb
Adam Balachowski <adam.balachowski@bertelsmann.de>
2022-12-12 15:43:51 +0100

    chore: check husky

--- apps/spark-app/pages/[[...app]].tsx
+++ apps/spark-app/pages/[[...app]].tsx
@@ -1,21 +1,16 @@
 import { useEffect, useState } from 'react';
 
 // The one to catch it all
-const Spark = () => <h1>SPARK</h1>;
+const Spark = () => <h1>CoreMedia SPARK Example</h1>;
 
 export default function App() {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    console.log({ isMounted });
     setIsMounted(true);
-    console.log({ isMounted });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!isMounted) {
-    return null;
-  }
+  if (!isMounted) return null;
 
   return <Spark />;
 }
