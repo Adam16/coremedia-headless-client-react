ba42c7ba2f93823f0225ad983ef2b987a541a3f4
Adam Balachowski <adam.balachowski@bertelsmann.de>
2022-12-12 14:59:48 +0100

    feat: adds husky eslint conf

--- apps/spark-app/pages/[[...app]].tsx
+++ apps/spark-app/pages/[[...app]].tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
 
-const Spark = () => <h1>SPARK</h1>
+// The one to catch it all
+const Spark = () => <h1>SPARK</h1>;
 
 export default function App() {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true)
-  }, [])
+    setIsMounted(true);
+  }, []);
 
   if (!isMounted) return null;
 
-  return <Spark />
+  return <Spark />;
 }
