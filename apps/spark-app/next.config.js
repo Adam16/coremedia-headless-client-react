e94bc1c0ee1104c54e7a54ac575a9f4ad6f7089c
Adam Balachowski <adam.balachowski@bertelsmann.de>
2022-12-12 15:00:23 +0100

    feat: adds caas rewrite

--- apps/spark-app/next.config.js
+++ apps/spark-app/next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const caasDestPath = process.env.VITE_API_ENDPOINT || "http://localhost:4000";
+
+// alternative: https://github.com/stegano/next-http-proxy-middleware
+async function rewrites() {
+  return [
+    {
+      source: "/caas",
+      destination: `${caasDestPath}`,
+    },
+  ];
+}
+
+/**
+ * App configuration
+ * config subpath point to this Next.js app. @see: https://nextjs.org/docs/migrating/incremental-adoption#strategies
+ * rewrites for caas
+ */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/ssr' // config subpath point to this Next.js app. @see: https://nextjs.org/docs/migrating/incremental-adoption#strategies
+  // basePath: '/ssr',
+  rewrites,
 }
 
 module.exports = nextConfig
